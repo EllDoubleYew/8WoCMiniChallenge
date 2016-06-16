da0ab1cbf6c180d1e051ed07ee5eb48bc205d816
EvanWiederspan <evan@wiederspan.org>
2016-06-16 15:30:46 -0400

    Added support for multiple books

--- main.js
+++ main.js
@@ -16,10 +16,12 @@ Opentip.styles.word = {
 //INITIALIZATION
 var chapterNumber = 1;
 var numOfChapters;
+var curBook;
 var book;
+var bookLookup = {};
 var lexicon = {};
-
-getScripture('Ephesians.json');
+getBookList();
+//getScripture('Ephesians.json');
 
 //go forward a chapter when clicked
 $("#nextChapter").click(function(){
@@ -51,7 +53,13 @@ $("#goToChapter").click(function(){
 		displayScripture(book, chapterNumber);
 	}
 	console.log(chapterNumber);
-})
+});
+
+$("#bookSelect").change(function(ev) {
+	var newBook = $("#bookSelect option:selected").text();
+	if (curBook != newBook)
+		switchBook(newBook);
+});
 
 // Checks for new selection whenever the mouse is released
 $("#scripture").mouseup(function(ev) {
@@ -121,73 +129,147 @@ $("#searchForm").submit(function(ev){
 //AJAX CALLS FOR BOOK AND LEXICON
 //First call gets and parses our JSON as well as adds metadata
 function getScripture(url){
-	$.ajax({
-	url: url,
-	dataType: 'json',
-	type: 'get',
-	cache: false,
-	success: function(data){
-
-		//This regex finds our strong numbers
-		var reg = /[^A-Za-z0-9 -]+ G[0-9]{2,6}/g;
-		var result = [];
-
-		//Iteration goes through chapters then verses
-		$.each(data.Ephesians, function(chap, verses) {
-	      $.each(verses, function(verse, text) {
-
-	      	//We remove all the text in {} and remove the []
-	      	text = text.replace(/{.*}|[\[\]]/g, "");
-
-	      	//Placing the verse number in our span so that we can add it to the users clip board later
-	      	var newVerse = "<span verse='" + verse + "'>";
-
-	      	//Adds the strong number to a strong container on the span and adds places the word in the wrapper
-	      	while(result = reg.exec(text)){
-	      		var a = result[0].split(" ");
-	      		newVerse += "<span strong ='" + a[1].substring(1) + "'>" + a[0] + " </span>";
-	      	}
-
-	      	//Closing our span tag for the verse
-	      	newVerse += "</span>"
-	        data.Ephesians[chap][verse] = newVerse;
-	      });
-	    });
-
-		//Update the view with data we got from our AJAX call
-		book = data;
-		numOfChapters = Object.keys(book.Ephesians).length;
-		displayScripture(data, chapterNumber);
-	},
-	error: function(err) {
-		alert("Could not get Epheisans JSON data");
-	}
-});
-
+// 	$.ajax({
+// 	url: url,
+// 	dataType: 'json',
+// 	type: 'get',
+// 	cache: false,
+// 	success: function(data){
+//
+// 		This regex finds our strong numbers
+// 		var reg = /[^A-Za-z0-9 -]+ G[0-9]{2,6}/g;
+// 		var result = [];
+//
+// 		//Iteration goes through chapters then verses
+// 		$.each(dat, function(chap, verses) {
+// 	      $.each(verses, function(verse, text) {
+//
+// 	      	//We remove all the text in {} and remove the []
+// 	      	text = text.replace(/{.*}|[\[\]]/g, "");
+//
+// 	      	//Placing the verse number in our span so that we can add it to the users clip board later
+// 	      	var newVerse = "<span verse='" + verse + "'>";
+//
+// 	      	//Adds the strong number to a strong container on the span and adds places the word in the wrapper
+// 	      	while(result = reg.exec(text)){
+// 	      		var a = result[0].split(" ");
+// 	      		newVerse += "<span strong ='" + a[1].substring(1) + "'>" + a[0] + " </span>";
+// 	      	}
+//
+// 	      	//Closing our span tag for the verse
+// 	      	newVerse += "</span>"
+// 	        data.Ephesians[chap][verse] = newVerse;
+// 	      });
+// 	    });
+//
+// 		//Update the view with data we got from our AJAX call
+// 		book = data;
+// 		numOfChapters = Object.keys(book.Ephesians).length;
+// 		displayScripture(data, chapterNumber);
+// 	},
+// 	error: function(err) {
+// 		alert("Could not get Epheisans JSON data");
+// 	}
+// });
 
 //This call gets and structures the lexicon
-$.ajax({
-	url: 'lexicon-eph-english.json',
-	dataType: 'json',
-	type: 'get',
-	cache: false,
+// $.ajax({
+// 	url: 'lexicon-eph-english.json',
+// 	dataType: 'json',
+// 	type: 'get',
+// 	cache: false,
+//
+// 	//Here we read in our lexicon and place it into a new structure
+// 	//TODO: EVAN could you maybe explain this better?
+// 	success: function(data) {
+//
+// 	},
+// 	error: function(err) {
+// 		alert("Could not get lexicon!");
+// 	}
+// });
+}
 
-	//Here we read in our lexicon and place it into a new structure
-	//TODO: EVAN could you maybe explain this better?
-	success: function(data) {
-		for (var i = 0; i < data.length; i++) {
-			lexicon[data[i].strongs] = data[i];
+function switchBook(bookName) {
+	var newBook = bookList[bookName];
+	// double ajax call!
+	$.when($.getJSON('data/' + newBook.Text), $.getJSON('data/' + newBook.Lex)).then(function(bookData, lexData) {
+		// update book data
+		var reg = /[^A-Za-z0-9 -]+ G[0-9]{2,6}/g;
+		var result = [];
+		// yeah im not proud of this but fingers crossed it works
+		var dat = bookData[0];
+		dat = dat[function() { for (var a in dat) {return a;}}()];
+		//Iteration goes through chapters then verses
+		$.each(dat, function(chap, verses) {
+				$.each(verses, function(verse, text) {
+
+					//We remove all the text in {} and remove the []
+					text = text.replace(/{.*}|[\[\]]/g, "");
+
+					//Placing the verse number in our span so that we can add it to the users clip board later
+					var newVerse = "<span verse='" + verse + "'>";
+
+					//Adds the strong number to a strong container on the span and adds places the word in the wrapper
+					while(result = reg.exec(text)){
+						var a = result[0].split(" ");
+						newVerse += "<span strong ='" + a[1].substring(1) + "'>" + a[0] + " </span>";
+					}
+
+					//Closing our span tag for the verse
+					newVerse += "</span>"
+					dat[chap][verse] = newVerse;
+
+				});
+			});
+			// wipe book and replace it
+		book = {};
+		book = dat;
+		curBook = bookName;
+		numOfChapters = Object.keys(dat).length;
+		// update lexicon
+		lexicon = {};
+			for (var i = 0; i < lexData[0].length; i++) {
+				lexicon[lexData[0][i].strongs] = lexData[0][i];
+			}
+			$("#bookTitle").text(bookName + " in Greek");
+			displayScripture(book, 1, 1, 1, 1);
+	}, function(err) {
+		console.log("Could not get book data for " + bookName);
+	});
+}
+
+function getBookList() {
+	$.ajax({
+		url: 'data/books.json',
+		dataType: 'json',
+		type: 'get',
+		cache: false,
+		success: function(data) {
+				bookList = data;
+				var options = "";
+				for (book in data) {
+					options += "<option value='1'>" + book + "</option>";
+				}
+				if (options != "") {
+					$("#bookSelect").html(options);
+					switchBook($("#bookSelect option")[0].innerHTML);
+				}
+				else {
+					console.log("Error: Could not load books");
+				}
+
+		},
+		error: function(err) {
+			alert("Error: Could not fetch book list");
 		}
-	},
-	error: function(err) {
-		alert("Could not get lexicon!");
-	}
-});
+	});
 }
 
 //Displays scripture to the screen given data and a chapter
 function displayScripture(data, startChap, startVerse, endChap, endVerse){
 	var chapterString = "";
+	if (startChap > numOfChapters) return;
 	if (endChap == undefined) endChap = startChap;
 	if (endChap < startChap) { // swap if need b
 		var temp = endChap;
@@ -197,15 +279,15 @@ function displayScripture(data, startChap, startVerse, endChap, endVerse){
 		endVerse = startVerse;
 		startVerse = temp;
 	}
-	for (var chap = startChap; chap <= endChap; chap++) {
+	for (var chap = startChap; chap <= endChap && chap <= numOfChapters; chap++) {
 		chapterString += '<span chapter="' + chap + '" verse="1" class="chapNum">' + chap + '</span> ';
-		for(verse in data["Ephesians"][chap]){
+		for(verse in data[chap]){
 			chapterString += '<span verse="' + verse + '" class="verseNum">' + verse + '</span> ';
-			chapterString += data["Ephesians"][chap][verse];
+			chapterString += data[chap][verse];
 		}
 	}
 
-	$("#chapter").html("Chapter " + startChap + ((startChap == endChap) ? "" : ("-" + endChap)));
+	$("#chapter").html("Chapter " + startChap + (startChap == endChap ? "" : ("-" + (endChap > numOfChapters ? numOfChapters : endChap))));
 	$("#scripture").html(chapterString);
 }
 
